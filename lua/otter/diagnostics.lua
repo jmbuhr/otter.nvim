493cf4f68c986165b66b4c42e98b1a7095ea7177
Jannik Buhr <jannik.m.buhr@gmail.com>
2025-02-01 11:56:19 +0100

    debug #20

--- lua/otter/diagnostics.lua
+++ lua/otter/diagnostics.lua
@@ -5,39 +5,49 @@ local keeper = require("otter.keeper")
 M = {}
 M.setup = function(main_nr)
   ---@type integer[]
-  local nss = {}
+  keeper.rafts[main_nr].diagnostics_namespaces = {}
   for lang, bufnr in pairs(keeper.rafts[main_nr].buffers) do
     local ns = api.nvim_create_namespace("otter-lang-" .. lang)
-    nss[bufnr] = ns
+    keeper.rafts[main_nr].diagnostics_namespaces[bufnr] = ns
   end
-  keeper.rafts[main_nr].diagnostics_namespaces = nss
 
   local sync_diagnostics = function(args)
-    if vim.tbl_contains(vim.tbl_values(keeper.rafts[main_nr].buffers), args.buf) then
-      local diags = args.data.diagnostics
-      vim.diagnostic.reset(nss[args.buf], main_nr)
-      if config.cfg.handle_leading_whitespace then
-        for _, diag in ipairs(diags) do
-          local offset = keeper.get_leading_offset(diag.lnum, main_nr)
-          diag.col = diag.col + offset
-          diag.end_col = diag.end_col + offset
-        end
-      end
-      vim.diagnostic.set(nss[args.buf], main_nr, diags, {})
+    local otter_nr = args.buf
+    if not vim.tbl_contains(vim.tbl_values(keeper.rafts[main_nr].buffers), otter_nr) then
+      -- diagnostics are not from an otter buffer
+      return
     end
+    vim.print(args.buf)
+    vim.print(args.data.diagnostics)
+    -- reset the diagnostics of the otter buffer in the main buffer
+    vim.diagnostic.reset(keeper.rafts[main_nr].diagnostics_namespaces[otter_nr], main_nr)
+
+    ---@type vim.Diagnostic[]
+    local diags = args.data.diagnostics
+    if config.cfg.handle_leading_whitespace then
+      for _, diag in ipairs(diags) do
+        local offset = keeper.get_leading_offset(diag.lnum, main_nr)
+        diag.col = diag.col + offset
+        diag.end_col = diag.end_col + offset
+      end
+    end
+    vim.diagnostic.set(keeper.rafts[main_nr].diagnostics_namespaces[otter_nr], main_nr, diags, {})
   end
 
-  local group = api.nvim_create_augroup("OtterDiagnostics" .. main_nr, {})
+  local diagnostics_augroup = api.nvim_create_augroup("OtterDiagnostics" .. main_nr, {})
+  keeper.rafts[main_nr].diagnostics_group = diagnostics_augroup
+
   api.nvim_create_autocmd("DiagnosticChanged", {
-    group = group,
+    group = diagnostics_augroup,
     callback = sync_diagnostics,
   })
-  keeper.rafts[main_nr].diagnostics_group = group
 
   api.nvim_create_autocmd(config.cfg.lsp.diagnostic_update_events, {
     buffer = main_nr,
-    group = group,
+    group = diagnostics_augroup,
     callback = function(_)
+      -- syncing the contents of the otter buffers will
+      -- trigger the diagnostic update events afterwards
       keeper.sync_raft(main_nr)
     end,
   })

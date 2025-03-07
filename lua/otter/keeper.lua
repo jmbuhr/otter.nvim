298c78c3d0b8c8ea5a731ed4e11e5e47a0b0d4e9
Jannik Buhr <jannik.m.buhr@gmail.com>
2025-03-07 08:47:15 +0100

    fix: match error message for textlock and catch

--- lua/otter/keeper.lua
+++ lua/otter/keeper.lua
@@ -392,20 +392,18 @@ keeper.sync_raft = function(main_nr, language)
   --- and can be delayed until the textlock is released.
   --- The lsp request should still be valid
   local function do_with_maybe_texlock(callback)
-    local texlock_err_msg = 'Vim(normal):E5556: API call: E565: Not allowed to change text or change window'
+    local texlock_err_msg = 'E565: Not allowed to change text or change window'
     local success, result = pcall(callback)
     if success then
       return "success"
     end
 
-    vim.notify_once("[otter.nvim] Hi there! You triggered an LSP request that is routed through otter.nvim while textlock is active. We would like to fix this, but need to find the exact form of the error message to match against. Please be so kind and open an issue with how you triggered this and the error object below:", vim.log.levels.WARN)
-    vim.notify_once(vim.inspect(result), vim.log.levels.WARN)
-
     if result == texlock_err_msg then
       vim.schedule(callback)
       return "textlock_active"
     else
-      error(result)
+      vim.notify_once("[otter.nvim] Hi there! You triggered an LSP request that is routed through otter.nvim while textlock is active. We would like to fix this, but need to find the exact form of the error message to match against. Please be so kind and open an issue with how you triggered this and the error object below:", vim.log.levels.WARN)
+      vim.notify_once(vim.inspect(result), vim.log.levels.WARN)
       return "error"
     end
   end
